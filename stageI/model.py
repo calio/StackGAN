444266f7879ff7444512199ce579ae80787af918
JZ <vipcalio@gmail.com>
2017-06-12 07:43:19 +0000

    Add deep image encoder

--- stageI/model.py
+++ stageI/model.py
@@ -43,7 +43,7 @@ class CondGAN(object):
                 self.discriminator_template = self.discriminator()
         elif cfg.GAN.NETWORK_TYPE == "deep":
             with tf.variable_scope("d_net"):
-                self.d_encode_img_template = self.d_encode_image()
+                self.d_encode_img_template = self.d_encode_image_simple()
                 self.d_context_template = self.context_embedding()
                 self.discriminator_template = self.discriminator()
         else:
@@ -270,6 +270,38 @@ class CondGAN(object):
              apply(leaky_rectify, leakiness=0.2).
              custom_conv2d(self.df_dim * 4, k_h=4, k_w=4).
              conv_batch_norm().
+             custom_conv2d(self.df_dim * 8, k_h=4, k_w=4).
+             conv_batch_norm())
+        node1_1 = \
+            (node1_0.
+             custom_conv2d(self.df_dim * 2, k_h=1, k_w=1, d_h=1, d_w=1).
+             conv_batch_norm().
+             apply(leaky_rectify, leakiness=0.2).
+             custom_conv2d(self.df_dim * 2, k_h=3, k_w=3, d_h=1, d_w=1).
+             conv_batch_norm().
+             apply(leaky_rectify, leakiness=0.2).
+             custom_conv2d(self.df_dim * 8, k_h=3, k_w=3, d_h=1, d_w=1).
+             conv_batch_norm())
+
+        node1 = \
+            (node1_0.
+             apply(tf.add, node1_1).
+             apply(leaky_rectify, leakiness=0.2))
+
+        return node1
+
+    def d_encode_image_deep(self):
+        node1_0 = \
+            (pt.template("input").
+             custom_conv2d(self.df_dim, k_h=4, k_w=4).
+             conv_batch_norm().
+             apply(leaky_rectify, leakiness=0.2).
+             custom_conv2d(self.df_dim * 2, k_h=4, k_w=4).
+             conv_batch_norm().
+             apply(leaky_rectify, leakiness=0.2).
+             custom_conv2d(self.df_dim * 4, k_h=4, k_w=4).
+             conv_batch_norm().
+             apply(leaky_rectify, leakiness=0.2).
              custom_conv2d(self.df_dim * 8, k_h=4, k_w=4).
              conv_batch_norm())
         node1_1 = \
